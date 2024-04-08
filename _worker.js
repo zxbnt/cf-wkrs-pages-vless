e37e53849e1b3d0bdd1ed23e28e9d855a04f9ed8
zxbnt <zxbnt@hotmail.com>
2024-04-08 18:05:00 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '27a086d2-f58f-11ee-a951-0242ac120002';
+let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 
 //const proxyIPs = ["workers.cloudflare.cyou"]; // 
 const proxyIPs = ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org', "workers.cloudflare.cyou"];
